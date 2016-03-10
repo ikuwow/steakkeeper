f99fdb1e2c8e552fcc6a839f488e931c701b7c18
Ikuo Degawa <degawa@ikuwow.com>
2016-03-10 23:48:40 +0900

    Add dashboard action

---
+++ tests/TestCase/Controller/DashboardControllerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\UsersController;
+use Cake\TestSuite\IntegrationTestCase;
+
+/**
+ * App\Controller\DashboardController Test Case
+ */
+class DashboardControllerTest extends IntegrationTestCase
+{
+    /**
+     * testIndex method
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->session($this->_setUserSession());
+        $this->get('/');
+        $this->assertResponseOk();
+    }
+
+    /**
+     * Login method in test
+     *
+     * @return array
+     */
+    protected function _setUserSession()
+    {
+        return [
+            'Auth' => [
+                'User' => [
+                    'id' => 100,
+                    'email' => 'john.doe@crm.com',
+                    'name' => 'testuser',
+                    'created' => '2015-04-01 22:26:51',
+                    'modified' => '2015-04-01 22:26:51'
+                ]
+            ]
+        ];
+    }
+}
